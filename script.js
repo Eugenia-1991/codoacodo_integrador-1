174d149784b1f8f29da4099ae21a2e00b03d4c3d
eugenia <colmeneroeugenia@gmail.com>
2022-11-07 15:08:32 -0300

    alerta

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ let descuentoEstudiante = 0.80;
 let descuentoTrainee = 0.50;
 let descuentoJunior = 0.15;
 
-
+//nueva
+const msj =document.getElementById('mensaje');
 
 const cantidad = document.getElementById('cantidad');
 const categoria = document.getElementById('categoriaSelect');
@@ -21,7 +22,7 @@ function total() {
   let totalvalor=(cantidad.value) * valorTicket;
   if (categoria.value == 0) {
     totalvalor = totalvalor - (totalvalor * descuentoEstudiante);
-   }
+   } 
   else if (categoria.value == 1) {
     totalvalor = totalvalor - (totalvalor * descuentoTrainee); 
   }
@@ -30,16 +31,37 @@ function total() {
   } else{
         totalvalor = totalvalor 
       }
-  p.innerHTML = `Total a pagar: ${totalvalor}`;
+  p.innerHTML = `Total a pagar: $ ${totalvalor}`;
 }
+
+
+function Felicitaciones(){
+  if(categoria.value == 0) {
+  msj.innerHTML = "<b>¡Felicitaciones!</b> Utilizaste el descuento del 80%  de la categoria estudiante."
+  }
+  else if(categoria.value == 1) {
+    msj.innerHTML = "<b>¡Felicitaciones!</b> Utilizaste el descuento del 50%  de la categoria Trainee."
+    }
+    else if(categoria.value == 2) {
+      msj.innerHTML = "<b>¡Felicitaciones!</b> Utilizaste el descuento del 15%  de la categoria Junior."
+      }
+   else {
+    msj.innerHTML = "<b>¡Upps!</b> No ingresaste ninguna categoria para acceder a los descuentos!"
+        }
+        
+      
+}
+
+botonCalcular.addEventListener("click", Felicitaciones);
 
 //funcion al boton
 botonCalcular.addEventListener("click", total);
 
 // boton reset
 botonBorrar.addEventListener('click', ()=>{
- 
- formulario.reset()
+  formulario.reset()
+  p.innerHTML = "Total a pagar: $"
+  msj.innerHTML = "<i>Ingresa tu categoria para aplicar a los mejores <b>descuentos</b>!</i>"
 
 });
 
